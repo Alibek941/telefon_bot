302b78e1eae84f937e7fde6f243292eaac175f8d
Alibek941 <alibekerkayev3@gmail.com>
2026-09-22 13:35:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ SUPER_ADMIN_ID = 6738533029  # To'liq boshqaruv
 PRODUCT_ADMIN_ID = 5156453500  # Faqat mahsulot boshqaruvi
 
 IPHONE_MODELS = [
-    "iPhone 11", 
-    "iPhone 12", "iPhone 12 Pro Max",
+    "iPhone 11", "iPhone 11 Pro", "iPhone 11 Pro Max",
+    "iPhone 12", "iPhone 12 Pro", "iPhone 12 Pro Max",
     "iPhone 13", "iPhone 13 Pro", "iPhone 13 Pro Max",
-    "iPhone 14", "iPhone 14 Pro", "iPhone 14 Pro Max",
-    "iPhone 15", "iPhone 15 Pro", "iPhone 15 Pro Max",
-    "iPhone 16", "iPhone 16 Pro", "iPhone 16 Pro Max",
+    "iPhone 14", "iPhone 14 Plus", "iPhone 14 Pro", "iPhone 14 Pro Max",
+    "iPhone 15", "iPhone 15 Plus", "iPhone 15 Pro", "iPhone 15 Pro Max",
+    "iPhone 16", "iPhone 16 Plus", "iPhone 16 Pro", "iPhone 16 Pro Max",
     "iPhone 17", "iPhone 17 Air", "iPhone 17 Pro", "iPhone 17 Pro Max",
     "iPhone 18", "iPhone 18 Air", "iPhone 18 Pro", "iPhone 18 Pro Max"
 ]
